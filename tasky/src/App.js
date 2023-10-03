fb30b55dece3ee441b81ec3d8de122ec2597d634
Evan Hearne <evanhearne02@gmail.com>
2023-10-03 14:51:42 +0100

    added priority

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -11,15 +11,16 @@ import Task from './components/Task';
 function App() {
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false },
-      { id: 3, title: "Tidy up", deadline: "Today", done: false}
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false, priority:"Low" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false, priority:"Medium" },
+      { id: 3, title: "Tidy up", deadline: "Today", done: false, priority:"High"}
     ]
   });
   const [ formState, setFormState ] = useState({
     title: "",
     description: "",
-    deadline: ""
+    deadline: "",
+    priority: ""
   });
   const formChangeHandler = (event) => {
     let form = {...formState};
@@ -33,6 +34,9 @@ function App() {
           break;
       case "deadline":
           form.deadline = event.target.value;
+          break;
+      case "priority":
+          form.priority = event.target.value;
           break;
       default:
           form = formState;
@@ -96,6 +100,7 @@ function App() {
                 key={task.id}
                 markDone = {() => doneHandler(index)}
                 deleteTask = {() => deleteHandler(index)}
+                priority = {task.priority}
               />
           ))}
         </Grid>
